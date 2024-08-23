8f96a2db4b2b0fe37f76e4a65dfb40a26d6797ae
unknown <lyhoangdong>
2024-08-23 21:46:51 +0700

    Video 51

---
+++ src/components/todo/TodoApp.jsx
@@ -0,0 +1,46 @@
+import './todo.css'
+import TodoData from './TodoData'
+import TodoNew from './TodoNew'
+import reactLogo from '../../assets/react.svg'
+import { useState } from 'react'
+const TodoApp = () => {
+    const [todoList, setTodoList] = useState([
+        // { id: 1, name: "Learning react" },
+        // { id: 2, name: "Warching youtube" }
+    ])
+
+    const addNewTodo = (name) => {
+        const newTodo = {
+            id: randomIntFromInterval(1, 1000000),
+            name: name
+        }
+        setTodoList([...todoList, newTodo])
+    }
+
+    const randomIntFromInterval = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    const deleteTodo = (id) => {
+        const newTodo = todoList.filter(item => item.id !== id)
+        setTodoList(newTodo);
+        console.log(">>>Check newtodo: ", JSON.stringify(newTodo))
+    }
+    return (
+        <div className="todo-container">
+            <div className="todo-title"> Todo List</div>
+            <TodoNew
+                addNewTodo={addNewTodo}
+            />
+            {todoList.length > 0 ?
+                <TodoData
+                    todoList={todoList}
+                    deleteTodo={deleteTodo}
+                /> : <div className='todo-image' >
+                    <img src={reactLogo} className='logo' />
+                </div>
+            }
+        </div>
+    )
+}
+export default TodoApp;
